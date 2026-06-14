e2bbf02a45348a8b3c15401080836ba8cae26631
jan-nav <navratill.jann@gmail.com>
2026-06-14 13:49:37 +0200

    ADD: Docstrings for functions.

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import requests, sys, csv
 from bs4 import BeautifulSoup
 
 def get_page(url):
+    """
+    Downloads the webpage from the provided URL and returns a BS object.
+    """
     page = requests.get(url)
     page.raise_for_status()
     soup = BeautifulSoup(page.text, 'html.parser')
     return soup
 
 def check_args(args):
+    """
+    Check the validity of provided arguments: their number and if a correct URL has been provided.
+    Upon valid check, returns URL and file name.
+    """
     if len(args) != 3:
         sys.exit("Error: Incorrect number of arguments")
     url = args[1]
@@ -17,6 +24,10 @@ def check_args(args):
     return url, file_name
 
 def get_munis(soup):
+    """
+    Searches for tables and cells containing sought after information (code of the municipality and its name).
+    Returns a list of triplets: municipality code, name and complete URL address for accessing voting statistics.
+    """
     munis = []
     rows = soup.find_all('tr')
     base_url = "https://volby.cz/pls/ps2017nss/"
@@ -34,7 +45,11 @@ def get_munis(soup):
             munis.append((code, name, base_url + link))
     return munis
 
-def get_town_stats(soup):
+def get_muni_stats(soup):
+    """
+    Extracts data from a specific table containing the sought out voting statistics.
+    Returns a dictionary containing the number of voters, issued envelopes and valid votes.
+    """
     stats = {"Počet voličů" : "",
              "Vydané obálky" : "",
              "Platné hlasy" : ""}
@@ -49,6 +64,10 @@ def get_town_stats(soup):
     return stats
 
 def get_party_stats(soup):
+    """
+    Returns a dictionary containing a complete list of political parties and their respective
+    vote counts.
+    """
     all_votes = {}
     for table in soup.find_all("table", {"class" : "table"}):
         rows = table.find_all("tr")
@@ -63,8 +82,12 @@ def get_party_stats(soup):
     return all_votes
 
 def data_compiler(code, name, url):
+    """
+    Compiles the previous functions to create a cohesive dataset for an individual municipality and
+    returns a dictionary.
+    """
     soup = get_page(url)
-    town = get_town_stats(soup)
+    town = get_muni_stats(soup)
     votes = get_party_stats(soup)
     row = {"Kód obce": code, "Název obce" : name}
     row.update(town)
@@ -72,6 +95,11 @@ def data_compiler(code, name, url):
     return row
 
 def csv_writer(file_name, rows):
+    """
+    Creates a CSV file with a name provided on a command line.
+    Apart from the fixed columns shared between municipalities, columns are added
+    dynamically based on the voted for parties.
+    """
     if not rows:
         return
     columns = ["Kód obce", "Název obce", "Počet voličů", "Vydané obálky", "Počet platných hlasů"]
@@ -86,6 +114,10 @@ def csv_writer(file_name, rows):
         writer.writerows(rows)
 
 def main():
+    """
+    Main function of the program; ensures that data from all gathered municipalities
+    are written into the CSV file.
+    """
     url, file_name = check_args(sys.argv)
     page = get_page(url)
     munis = get_munis(page)
